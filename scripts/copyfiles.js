20425a0d1d88b6a822763875dca920b3423d7956
toptobes <kavinpg@gmail.com>
2022-06-27 16:24:21 -0500

    Fixed a working directory issue

--- scripts/copyfiles.js
+++ scripts/copyfiles.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
-const dest = "C:\\Users\\wanna\\AppData\\Roaming\\.minecraft\\config\\ChatTriggers\\modules\\CarburettorUtilities\\"
+const dest = "C:/Users/wanna/AppData/Roaming/.minecraft/config/ChatTriggers/modules/CarburettorUtilities/"
+const src = path.join(__dirname, '..', 'src/')
 
 if (!fs.existsSync(dest)){
     fs.mkdirSync(dest);
 }
 
-fs.readdirSync("../src").forEach(file => {
+fs.readdirSync(src).forEach(file => {
     copyFileToCTModule(file)
 });
 
 function copyFileToCTModule(file) {
-    fs.copyFile(`../src/${file}`, dest + file, err => {
+    fs.copyFile(src + file, dest + file, err => {
         if (err) throw err;
         console.log(`${file} was copied to destination`);
     });
