61c67d03f4bea6006bb49ce6b6c8f17d8f76cc45
toptobes <kavinpg@gmail.com>
2022-08-06 23:29:12 -0500

    Cleaned up hover message for hypixel chat stats

--- src/HypixelChatStats.js
+++ src/HypixelChatStats.js
@@ -1,14 +1,19 @@
 export function startHypixelChatStats() {}
 
-register('chat', (_, star, name, message, event) => {
+register('chat', (_, prefixes, name, message, event) => {
     let chatMessage = ChatLib.getChatMessage(event);
+
+    const cms = chatMessage.split('[');
+    const rankColor = cms.length === 2 ? '§7' : cms[1].slice(-2);
+
+    name = name.replace(/§./g, '');
 
     chatMessage = new Message(
         new TextComponent(chatMessage)
-            .setClick("run_command", `/w +${name.slice(0, -2)}`)
-            .setHoverValue("Does /w +${name} on click, for use w/ Cubelify"),
+            .setClick("run_command", `/w +${name}`)
+            .setHoverValue(`§7Does /w +${rankColor + name}§7 on click, for use w/ Cubelify`),
     );
 
     ChatLib.chat(chatMessage);
     cancel(event);
-}).setCriteria('§${_}[${star}] ${name}: ${message}');
+}).setCriteria('§${_}[${prefixes}] ${name}: ${message}');
