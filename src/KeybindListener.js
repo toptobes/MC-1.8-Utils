62bb090739f15c8da12f9ec5472e479dfb97d206
toptobes <kavinpg@gmail.com>
2022-09-13 21:30:32 -0500

    sasf

--- src/KeybindListener.js
+++ src/KeybindListener.js
@@ -1,9 +1,3 @@
-let yKeyBind = getKeyBindFromKey(Keyboard.KEY_Y, 'Y - Incoming player');
-let uKeyBind = getKeyBindFromKey(Keyboard.KEY_U, 'U - Stay back');
-let kKeyBind = getKeyBindFromKey(Keyboard.KEY_K, 'K - /who');
-let lKeyBind = getKeyBindFromKey(Keyboard.KEY_L, 'L - /l (k1)');
-let mKeyBind = getKeyBindFromKey(Keyboard.KEY_M, 'M - /l (k2)');
-
 export function startKeybindListeners() {}
 
 function getKeyBindFromKey(key, description) {
@@ -15,6 +9,13 @@ function getKeyBindFromKey(key, description) {
 
     return mcKeyBind;
 }
+
+let yKeyBind = getKeyBindFromKey(Keyboard.KEY_Y, 'Y - Incoming player');
+let uKeyBind = getKeyBindFromKey(Keyboard.KEY_U, 'U - Stay back');
+let iKeyBind = getKeyBindFromKey(Keyboard.KEY_I, 'I - Coming back');
+let kKeyBind = getKeyBindFromKey(Keyboard.KEY_K, 'K - /who');
+let lKeyBind = getKeyBindFromKey(Keyboard.KEY_L, 'L - /l (k1)');
+let mKeyBind = getKeyBindFromKey(Keyboard.KEY_M, 'M - /l (k2)');
 
 let cycle = 0;
 register('tick', () => {
@@ -31,6 +32,9 @@ register('tick', () => {
         ChatLib.say('Stay back just in case');
     }
 
+    if (iKeyBind.isPressed()) {
+        ChatLib.say('I\'m coming back');
+    }
 
     if (kKeyBind.isPressed()) {
         ChatLib.say('/who');
--- src/MiscChatChecks.js
+++ src/MiscChatChecks.js
@@ -3,29 +3,19 @@
 export function startMiscChatChecks() {}
 
 register('chat', (message) => {
-    checkForAutoWelcomeBack(message);
+    // checkForAutoWelcomeBack(message);
     // checkForAutoReportInGuildChat(message);
     checkForAutoGG(message);
     // checkForAutoWhoForMMC(message);
-
-    if (message.includes("mefliolover321 was killed by")) {
-        setTimeout(() => {
-            ChatLib.say("/pc Owie.");
-        }, 1000);
-    }
-
-    if (message.includes("mefliolover321 fell into the void.")) {
-        setTimeout(() => {
-            ChatLib.say("/pc Youch.");
-        }, 1000);
-    }
 }).setCriteria('${message}');
 
+/*
 function checkForAutoWelcomeBack(message) {
     if (message.includes('Guild > ') && message.includes('joined')) {
         ChatLib.say(`/gc Welcome back, ${message.split(' ')[message.split(' ').length - 2]}`);
     }
 }
+*/
 
 /*function checkForAutoReportInGuildChat(message) {
     if (message.includes('§2Guild') && ['/report', '/cr', '/wdr'].some(value => message.includes(value))) {
